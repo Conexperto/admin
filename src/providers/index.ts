31a8c5049c38c102a0b6bd228bd459a76bf5cb63
Fernando Fernandez <frfernandezdev@gmail.com>
2021-10-15 15:29:41 -0400

    feat(providers): add index export

---
+++ src/providers/index.ts
@@ -0,0 +1,5 @@
+export { AuthProvider, useAuthContext } from "./auth";
+export type { AuthContextInterface } from "./auth";
+
+export { AppProvider, useAppContext } from "./app";
+export type { AppContextInterface } from "./app";
