3af48a3bdc610bf7316aaca5ee27a09ab43827fa
Fernando Fernandez <frfernandezdev@gmail.com>
2021-10-15 15:24:30 -0400

    feat(models): add index export

---
+++ src/models/index.ts
@@ -0,0 +1,6 @@
+export { Admin } from "./admin";
+export { Auth } from "./auth";
+export { Method } from "./method";
+export { Plan } from "./plan";
+export { Speciality } from "./speciality";
+export { User } from "./user";
