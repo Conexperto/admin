a7af7980ea344703437b3d87a791d256d0dee33e
Fernando Fernandez <frfernandezdev@gmail.com>
2021-10-16 16:16:38 -0400

    feat(navigations): add Login

--- src/navigations/unauthorized.tsx
+++ src/navigations/unauthorized.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import { MainUnauthorizedLayout } from "layouts";
+import { Login } from "pages";
 
 export const UnAuthorizedStack = (): JSX.Element => (
   <Switch>
     <MainUnauthorizedLayout>
-      <Route path="/" render={() => <div>UnAuthorizedStack</div>} />
+      <Route path="/" render={() => <Login />} />
     </MainUnauthorizedLayout>
   </Switch>
 );
