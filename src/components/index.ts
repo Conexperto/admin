e68a5fc35831ad459111235f23d1ddf0efcb12ac
Fernando Fernandez <frfernandezdev@gmail.com>
2021-10-16 16:17:10 -0400

    feat(navigations): update index export

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,5 @@
 export { default as AppBar } from "./appbar";
 export { default as Drawer } from "./drawer";
+export { default as TextFieldPassword } from "./textfield-password";
+export { default as ButtonLoading } from "./button-loading";
+export { default as Snackbar } from "./snackbar";
