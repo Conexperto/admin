170df17a045bd5fb4b6513c1bbced99e9d11d76e
Fernando Fernandez <frfernandezdev@gmail.com>
2021-10-16 20:02:34 -0400

    feat(navigations): add MainLayouts

--- src/navigations/authorized.tsx
+++ src/navigations/authorized.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
+import { MainLayout } from "layouts";
 
 export const AuthorizedStack = (): JSX.Element => (
   <Switch>
-    <Route path="/" render={() => <div>AuthorizedStack</div>} />
+    <MainLayout>
+      <Route path="/" render={() => <div>AuthorizedStack</div>} />
+    </MainLayout>
   </Switch>
 );
