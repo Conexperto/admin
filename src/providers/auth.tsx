66510ee1579298834b46290fa1703cb84bd1f3a2
Fernando Fernandez <frfernandezdev@gmail.com>
2021-10-17 16:05:37 -0400

    feat(provider): add useCallback for each method

--- src/providers/auth.tsx
+++ src/providers/auth.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { admin } from "firebase";
 import {
   browserSessionPersistence,
@@ -8,6 +8,7 @@ import {
   signInWithEmailAndPassword,
   User,
 } from "firebase/auth";
+import { Auth, IAuth } from "models";
 import { createContextHooks } from "hooks";
 import { ReactNode, useEffect, useState } from "react";
 import { useAppContext } from "./app";
@@ -15,7 +16,7 @@ import { useAppContext } from "./app";
 const { auth } = admin;
 
 export interface AuthContextInterface {
-  user: User | null | undefined;
+  user: IAuth | null | undefined;
   login: (email: string, password: string) => Promise<void>;
   logout: () => Promise<void>;
 }
@@ -26,38 +27,43 @@ type Props = {
   children?: ReactNode;
 };
 export const AuthProvider = ({ children }: Props) => {
-  const [user, setUser] = useState<User | null>();
-  const { toggleSnackbar, messageSnackbar } = useAppContext();
+  const [user, setUser] = useState<IAuth | null>();
+  const { toggleSnackbar, messageSnackbar, toggleLoader } = useAppContext();
 
-  async function login(email: string, password: string): Promise<void> {
-    try {
-      await setPersistence(auth, browserSessionPersistence);
-      await signInWithEmailAndPassword(auth, email, password);
-    } catch (err) {
-      console.error(err);
-      messageSnackbar("Oops, problemas con la autenticacion.");
-      toggleSnackbar(true);
-    }
-  }
+  const login = useCallback(
+    async (email: string, password: string): Promise<void> => {
+      try {
+        await setPersistence(auth, browserSessionPersistence);
+        await signInWithEmailAndPassword(auth, email, password);
+      } catch (err) {
+        console.error(err);
+        messageSnackbar("Oops, problemas con la autenticacion.");
+        toggleSnackbar(true);
+      }
+    },
+    [messageSnackbar, toggleSnackbar]
+  );
 
-  async function logout(): Promise<void> {
+  const logout = useCallback(async (): Promise<void> => {
     await auth.signOut();
-  }
+  }, []);
 
   useEffect(
     () =>
       onIdTokenChanged(auth, async (user: User | null): Promise<void> => {
+        toggleLoader(false);
         if (!user) {
           localStorage.removeItem("token");
           setUser(null);
           return;
         }
+
         const token = await getIdToken(user);
-        setUser(user);
+        setUser(new Auth({ uid: user.uid, a: user }));
         // required by HttpClient
         localStorage.setItem("token", token);
       }),
-    []
+    [toggleLoader]
   );
 
   useEffect(() => {
