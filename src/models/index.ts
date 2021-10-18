8db62ea35ba5e426b13fbbc9251b65eef0efa503
Fernando Fernandez <frfernandezdev@gmail.com>
2021-10-17 22:19:14 -0400

    feat(models): update index export

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,5 +1,6 @@
-export { Admin } from "./admin";
+export { Admin, Privileges } from "./admin";
 export { Auth } from "./auth";
+export type { IAuth } from "./auth";
 export { Method } from "./method";
 export { Plan } from "./plan";
 export { Speciality } from "./speciality";
