05068debaf7339d46c6be95ed497d661f329ff39
Fernando Fernandez <frfernandezdev@gmail.com>
2021-10-17 22:19:59 -0400

    feat(navigations): add profile route

--- src/navigations/authorized.tsx
+++ src/navigations/authorized.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import { MainLayout } from "layouts";
+import { Profile } from "pages";
 
 export const AuthorizedStack = (): JSX.Element => (
   <Switch>
     <MainLayout>
-      <Route path="/" render={() => <div>AuthorizedStack</div>} />
+      <Route path="/" exact render={() => <div>AuthorizedStack</div>} />
+      <Route path="/profile" render={() => <Profile />}></Route>
     </MainLayout>
   </Switch>
 );
