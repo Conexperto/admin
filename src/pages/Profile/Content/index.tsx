4ca0f93d4011d605168cdc651f4bf3b575e5efdf
Fernando Fernandez <frfernandezdev@gmail.com>
2021-10-19 07:26:39 -0400

    feat(pages): add request to server auth

--- src/pages/Profile/Content/index.tsx
+++ src/pages/Profile/Content/index.tsx
@@ -1,10 +1,41 @@
 import React from "react";
+import { useEffect, useCallback } from "react";
 import { Route, Redirect } from "react-router-dom";
 import { Box } from "@mui/material";
 import Credentials from "./Credentials";
 import Info from "./Info";
+import { useAuthContext } from "providers";
+import { IAuth, Auth } from "models";
+import { HttpAuth } from "../../../http";
 
 export default function Content(): JSX.Element {
+  const { user, setUser } = useAuthContext();
+
+  const currentUser = useCallback(
+    async function (http: HttpAuth) {
+      if (!user) return;
+      if (user?.b) return;
+
+      const currentUser = await http.currentUser();
+      setUser((prevState: IAuth | null | undefined) => {
+        if (!prevState) return null;
+
+        return new Auth({
+          uid: prevState.uid,
+          a: prevState.a,
+          b: currentUser.b,
+        });
+      });
+    },
+    [user, setUser]
+  );
+
+  useEffect(() => {
+    const http = new HttpAuth();
+    currentUser(http);
+    return () => (http.isCancel() ? http.cancel() : undefined);
+  }, [currentUser]);
+
   return (
     <Box mx={2} my={2}>
       <Route exact path="/profile">
