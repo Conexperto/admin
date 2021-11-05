48372899708747201b15ce5464e13fb849b5d000
Fernando Fernandez <frfernandezdev@gmail.com>
2021-11-04 22:24:12 -0400

    feat(providers): update index import

--- src/providers/index.ts
+++ src/providers/index.ts
@@ -3,3 +3,8 @@ export type { AuthContextInterface } from "./auth";
 
 export { AppProvider, useAppContext } from "./app";
 export type { AppContextInterface } from "./app";
+
+export { FabActonsProvider, useFabActionsContext } from "./fabactions";
+export type { FabActionsContextInterface } from "./fabactions";
+
+export { theme } from "./theme";
