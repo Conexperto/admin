a4e2188fa53b7def9842c0770893e0a47aaaecd5
Fernando Fernandez <frfernandezdev@gmail.com>
2021-11-04 22:19:41 -0400

    feat(hooks): update index imports

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,2 +1,4 @@
 export { createContextHooks } from "./create-context";
 export { useFormData } from "./form-data";
+export { useInterval } from "./interval";
+export { useTimeout } from "./timeout";
