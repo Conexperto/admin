47d6db33fae51f8154d4d4c05218c18e78e73fb0
Fernando Fernandez <frfernandezdev@gmail.com>
2021-11-04 22:17:28 -0400

    refactor(models): remove object auth

--- src/models/auth.ts
+++ src/models/auth.ts
@@ -1,37 +1,13 @@
-import { IAdmin, Privileges } from "./admin";
+import { IAdmin } from "./admin";
 import type { User } from "firebase/auth";
 
 interface UserRecord extends User {
   claims: { [key: string]: any };
 }
 
-export interface IAuth {
+export interface Auth {
   uid: string;
   a: UserRecord;
   b?: IAdmin;
   member?: string;
-}
-
-export class Auth implements IAuth {
-  public uid: string;
-  public a: UserRecord;
-  public b?: IAdmin;
-
-  constructor(payload: IAuth) {
-    this.uid = payload.uid;
-    this.a = payload.a;
-    this.b = payload.b;
-  }
-
-  public get auth(): UserRecord {
-    return this.a;
-  }
-
-  public get user(): IAdmin | undefined {
-    return this.b;
-  }
-
-  public get member(): string {
-    return Privileges[this.a.claims.access_level];
-  }
 }
--- src/models/index.ts
+++ src/models/index.ts
@@ -1,7 +1,6 @@
 export { Admin, Privileges } from "./admin";
 export type { IAdmin } from "./admin";
-export { Auth } from "./auth";
-export type { IAuth } from "./auth";
+export type { Auth } from "./auth";
 export { Method } from "./method";
 export { Plan } from "./plan";
 export { Speciality } from "./speciality";
