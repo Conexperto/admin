c2563c4b30b13ce79d9e54f01ea80c76b83f6684
Fernando Fernandez <frfernandezdev@gmail.com>
2021-11-04 22:30:53 -0400

    feat(pages): add FabActions callback

--- src/pages/Profile/Content/Credentials/index.tsx
+++ src/pages/Profile/Content/Credentials/index.tsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import type { SxProps } from "@mui/system";
-import { Box, Fab, TextField } from "@mui/material";
-import { Edit } from "@mui/icons-material";
+import { Box, TextField } from "@mui/material";
 import { TextFieldPassword } from "components";
-import { useAuthContext } from "providers";
+import { useAuthContext, useFabActionsContext } from "providers";
+import { useFormData } from "hooks";
 
 const styleForm: SxProps = {
   display: "flex",
@@ -16,46 +16,93 @@ const styleForm: SxProps = {
 
 export default function Credentials(): JSX.Element {
   const { user } = useAuthContext();
+  const [readOnly, setReadOnly] = useState<boolean>(true);
+  const { data, setData, handleChange } = useFormData({
+    email: "",
+    password: "",
+    displayName: "",
+    phoneNumber: "",
+  });
+  const { onEdit, onSave, onCancel, dispatch } = useFabActionsContext();
+
+  useEffect(() => {
+    if (!user) return;
+    if (!user.a) return;
+
+    setData({
+      email: user.a.email ?? "",
+      password: "",
+      displayName: user.a.displayName ?? "",
+      phoneNumber: user.a.phoneNumber ?? "",
+    });
+  }, [user, setData]);
+
+  useEffect(() => {
+    return () => {
+      dispatch("default");
+    };
+  }, [dispatch]);
+
+  useEffect(
+    () =>
+      onEdit(() => {
+        dispatch("action");
+        setReadOnly(false);
+      }),
+    [onEdit, dispatch]
+  );
+
+  useEffect(
+    () =>
+      onSave(() => {
+        dispatch("default");
+        setReadOnly(true);
+      }),
+    [onSave, dispatch]
+  );
+
+  useEffect(
+    () =>
+      onCancel(() => {
+        dispatch("default");
+        setReadOnly(true);
+      }),
+    [onCancel, dispatch]
+  );
 
   return (
     <Box>
       <Box component="form" sx={styleForm}>
         <TextField
           id="email"
-          value={user?.a.email ?? ""}
+          value={data.email}
+          onChange={handleChange("email")}
           InputProps={{
-            readOnly: true,
+            readOnly,
           }}
           label="Correo Electronico"
           variant="standard"
         />
-        <TextFieldPassword />
+        <TextFieldPassword readOnly={readOnly} />
         <TextField
           id="displayName"
-          value={user?.a.displayName ?? ""}
+          value={data.displayName}
           InputProps={{
-            readOnly: true,
+            readOnly,
           }}
           label="Nombre de Usuario"
           variant="standard"
         />
         <TextField
           id="phoneNumber"
-          value={user?.a.phoneNumber ?? ""}
+          value={data.phoneNumber}
           InputProps={{
-            readOnly: true,
+            readOnly,
           }}
           label="Telefono"
           variant="standard"
         />
       </Box>
-      <Fab
-        sx={{ position: "absolute", bottom: 16, right: 16 }}
-        aria-label="Editar"
-        color="primary"
-      >
-        <Edit />
-      </Fab>
     </Box>
   );
 }
