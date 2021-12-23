61d37ee56b1c156d1d49e62b9f8b24ffc6aae091
Fernando Fernandez <frfernandezdev@gmail.com>
2021-12-23 09:21:39 -0400

    feat(shared): added Checkbox atom

---
+++ src/modules/shared/presentation/atoms/Checkbox/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import MuiFormControlLabel, {
+  FormControlLabelProps as MuiFormControlLabelProps,
+} from "@mui/material/FormControlLabel";
+import MuiFormGroup from "@mui/material/FormGroup";
+import MuiCheckbox from "@mui/material/Checkbox";
+
+export type CheckboxProps = MuiFormControlLabelProps & {};
+const Checkbox: React.FC<CheckboxProps> = (props) => {
+  return (
+    <MuiFormGroup>
+      <MuiFormControlLabel {...props} control={<MuiCheckbox />} />
+    </MuiFormGroup>
+  );
+};
+
+export default Checkbox;
