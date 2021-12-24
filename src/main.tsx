1ef42d4c846cc3feb9fb05c3f77b58a5042c2e92
Fernando Fernandez <frfernandezdev@gmail.com>
2021-12-24 19:05:25 -0400

    feat(main): added CoreAppStore and App

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { CoreAppStore } from "./modules/core/infrastructure/store/CoreAppStore";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CoreAppStore>
+      <App />
+    </CoreAppStore>
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
