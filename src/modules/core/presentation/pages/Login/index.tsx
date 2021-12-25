b7b41c7dad2b9c1de7f2bd8c07c80ff487610464
Fernando Fernandez <frfernandezdev@gmail.com>
2021-12-25 15:41:10 -0400

    feat(core): added FormLogin to Login page

--- src/modules/core/presentation/pages/Login/index.tsx
+++ src/modules/core/presentation/pages/Login/index.tsx
@@ -1,10 +1,13 @@
 import React from "react";
+import FormLogin from "../../organisms/FormLogin";
 import TemplateUnauthorized from "../../templates/TemplateUnauthorized";
 
 const Login: React.FC = () => {
   return (
     <TemplateUnauthorized>
-      <div data-testid="page-login"></div>
+      <div data-testid="page-login">
+        <FormLogin />
+      </div>
     </TemplateUnauthorized>
   );
 };
