4e00f020e791116a83a3edef86dfff62b8db3916
Fernando Fernandez <frfernandezdev@gmail.com>
2021-12-26 13:35:48 -0400

    feat(shared): added FirebaseAuthClientFactory

---
+++ src/modules/shared/infrastructure/presistence/firebase/FirebaseAuthClientFactory.ts
@@ -0,0 +1,37 @@
+import type { FirebaseApp } from "@firebase/app";
+import type { Auth } from "@firebase/auth";
+import { getAuth, connectAuthEmulator } from "firebase/auth";
+import { Nullable } from "src/modules/shared/domain/Nullable";
+
+const env: { [key: string]: any } = {
+  admin: process.env.EMULATOR_ADMIN_HOST,
+  web: process.env.EMULATOR_WEB_HOST,
+};
+
+export class FirebaseAuthClientFactory {
+  private static clients: { [key: string]: Auth };
+
+  static createClient(contextName: string, app: FirebaseApp) {
+    let client = FirebaseAuthClientFactory.getClient(contextName);
+    if (!client) {
+      client = getAuth(app);
+
+      const emulator_host = env[contextName];
+      if (emulator_host) {
+        connectAuthEmulator(client, emulator_host);
+      }
+
+      FirebaseAuthClientFactory.registerClient(client, contextName);
+    }
+
+    return client;
+  }
+
+  static getClient(contextName: string): Nullable<Auth> {
+    return FirebaseAuthClientFactory.clients[contextName];
+  }
+
+  private static registerClient(client: Auth, contextName: string) {
+    FirebaseAuthClientFactory.clients[contextName] = client;
+  }
+}
