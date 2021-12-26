8b37a0c818e49d9e88dba0c66d4be76622e16249
Fernando Fernandez <frfernandezdev@gmail.com>
2021-12-26 13:37:10 -0400

    feat(core): added FirebaseCoreUserRepository

---
+++ src/modules/core/infrastructure/persistence/FirebaseCoreUserRepository.ts
@@ -0,0 +1,3 @@
+import { FirebaseUserRepository } from "src/modules/shared/infrastructure/presistence/firebase/FirebaseUserRepository";
+
+export class FirebaseCoreUserRepository extends FirebaseUserRepository {}
