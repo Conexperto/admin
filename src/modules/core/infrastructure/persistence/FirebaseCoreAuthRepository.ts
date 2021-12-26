1167289ae13eac604d9c63083a0087e6c443ccf6
Fernando Fernandez <frfernandezdev@gmail.com>
2021-12-26 13:36:54 -0400

    feat(core): added FirebaseCoreAuthRepository

---
+++ src/modules/core/infrastructure/persistence/FirebaseCoreAuthRepository.ts
@@ -0,0 +1,3 @@
+import { FirebaseAuthRepository } from "src/modules/shared/infrastructure/presistence/firebase/FirebaseAuthRepository";
+
+export class FirebaseCoreAuthRepository extends FirebaseAuthRepository {}
