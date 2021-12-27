8a5b510ac0718be9e81f0c7467b29d8a4cb0fe69
Fernando Fernandez <frfernandezdev@gmail.com>
2021-12-27 19:48:16 -0400

    feat(core): added new methods to CoreAuthBloc

--- src/modules/core/infrastructure/bloc/CoreAuthBloc.ts
+++ src/modules/core/infrastructure/bloc/CoreAuthBloc.ts
@@ -1,16 +1,49 @@
+import { User } from "firebase/auth";
 import { Bloc } from "src/modules/shared/infrastructure/bloc/Bloc";
+import { LocalStorageFactory } from "src/modules/shared/infrastructure/presistence/local-storage/LocalStorageFactory";
 import { CoreAuthState, initialAuthState } from "../../domain/CoreAuthState";
+import { FirebaseCoreAuthRepository } from "../persistence/FirebaseCoreAuthRepository";
 
-export class CoreAuthBloc extends Bloc<CoreAuthState> {
-  constructor(_initialAuthState: CoreAuthState = initialAuthState) {
+export class CoreAuthBloc extends Bloc<CoreAuthState<User>> {
+  constructor(
+    private repository: FirebaseCoreAuthRepository,
+    private localStore: LocalStorageFactory,
+    _initialAuthState: CoreAuthState<User> = initialAuthState
+  ) {
     super(_initialAuthState);
   }
 
-  signIn() {
-    this.changeState({ ...this.state, logged: true });
+  onAuthStateChanged() {
+    return this.repository.onAuthStateChanged((user) => {
+      if (!user) {
+        return this.changeState({ ...this.state, user: null });
+      }
+      this.changeState({ ...this.state, user });
+    });
   }
 
-  signOut() {
-    this.changeState({ ...this.state, logged: false });
+  onIdTokenChanged() {
+    return this.repository.onIdTokenChanged(async (user) => {
+      if (!user) {
+        return this.localStore.remove("token");
+      }
+      this.localStore.add("token", await user.getIdToken());
+    });
+  }
+
+  async signIn(email: string, password: string) {
+    try {
+      await this.repository.signInWithEmailAndPassword(email, password);
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  async signOut() {
+    try {
+      await this.repository.signOut();
+    } catch (e) {
+      console.error(e);
+    }
   }
 }
