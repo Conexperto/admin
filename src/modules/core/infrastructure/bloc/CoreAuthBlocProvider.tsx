92f7d9294d18c577c6a4aa36558e572dba69cb3f
Fernando Fernandez <frfernandezdev@gmail.com>
2021-12-27 19:49:47 -0400

    feat(core): added observable for auth state and toke state

--- src/modules/core/infrastructure/bloc/CoreAuthBlocProvider.tsx
+++ src/modules/core/infrastructure/bloc/CoreAuthBlocProvider.tsx
@@ -1,13 +1,19 @@
 import { ReactChild, ReactChildren, useEffect, useState } from "react";
 import createContextHook from "src/modules/shared/infrastructure/hooks/createContext";
+import { FirebaseAuthClientFactory } from "src/modules/shared/infrastructure/presistence/firebase/FirebaseAuthClientFactory";
+import { FirebaseClientFactory } from "src/modules/shared/infrastructure/presistence/firebase/FirebaseClientFactory";
 import { CoreAuthState } from "../../domain/CoreAuthState";
+import { FirebaseCoreAuthRepository } from "../persistence/FirebaseCoreAuthRepository";
 import { CoreAuthBloc } from "./CoreAuthBloc";
+import * as config from "../config/adminsdk.json";
+import { LocalStorageFactory } from "src/modules/shared/infrastructure/presistence/local-storage/LocalStorageFactory";
+import { User } from "firebase/auth";
 
 export const [useCoreAuth, Provider] =
-  createContextHook<{ bloc: CoreAuthBloc; state: CoreAuthState }>();
+  createContextHook<{ bloc: CoreAuthBloc; state: CoreAuthState<User> }>();
 
 export type CoreAuthBlocProviderProps = {
-  initialState?: CoreAuthState;
+  initialState?: CoreAuthState<User>;
   children: ReactChild | ReactChildren;
 };
 
@@ -15,11 +21,20 @@ export const CoreAuthBlocProvider: React.FC<CoreAuthBlocProviderProps> = ({
   initialState,
   children,
 }) => {
-  const bloc = new CoreAuthBloc(initialState);
-  const [state, setState] = useState<CoreAuthState>(bloc.state);
+  const firebaseClient = FirebaseClientFactory.createClient("admin", config);
+  const authClient = FirebaseAuthClientFactory.createClient(
+    "admin",
+    firebaseClient
+  );
+  const bloc = new CoreAuthBloc(
+    new FirebaseCoreAuthRepository(authClient),
+    new LocalStorageFactory(),
+    initialState
+  );
+  const [state, setState] = useState<CoreAuthState<User>>(bloc.state);
 
   useEffect(() => {
-    const stateSubscription = (state: CoreAuthState) => {
+    const stateSubscription = (state: CoreAuthState<User>) => {
       setState(state);
     };
 
@@ -27,6 +42,10 @@ export const CoreAuthBlocProvider: React.FC<CoreAuthBlocProviderProps> = ({
 
     return () => bloc.unsubscribe(stateSubscription);
   });
+
+  useEffect(() => bloc.onAuthStateChanged(), []);
+
+  useEffect(() => bloc.onIdTokenChanged(), []);
 
   return <Provider value={{ bloc, state }}>{children}</Provider>;
 };
