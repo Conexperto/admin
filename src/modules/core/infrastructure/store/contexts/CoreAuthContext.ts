6ef1375a341b970e02c786079bd5973b70c390d6
Fernando Fernandez <frfernandezdev@gmail.com>
2021-12-27 10:55:13 -0400

    refactor(core): remove store

--- src/modules/core/infrastructure/store/contexts/CoreAuthContext.ts
+++
@@ -1,6 +0,0 @@
-import { createContext } from "react";
-import { ICoreAuthContext } from "../interfaces/CoreAuthContext";
-
-export const CoreAuthContext = createContext<ICoreAuthContext>({
-  logged: false,
-});
