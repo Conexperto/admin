15a86705a85adb3a49ff9d487a1025c8011fb1c6
Fernando Fernandez <frfernandezdev@gmail.com>
2021-12-29 09:56:59 -0400

    test(core): remove params of wrap

--- src/modules/core/presentation/organisms/FormLogin/index.test.tsx
+++ src/modules/core/presentation/organisms/FormLogin/index.test.tsx
@@ -1,31 +1,26 @@
-import { ReactElement } from "react";
-import { LocationDescriptor } from "history";
-import { render, RenderOptions } from "@testing-library/react";
+import React from "react";
+import { render } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import FormLogin from ".";
-import {
-  CoreAuthBlocProvider,
-  CoreAuthBlocProviderProps,
-} from "src/modules/core/infrastructure/bloc/CoreAuthBlocProvider";
+import { CoreAuthBlocProvider } from "src/modules/core/infrastructure/bloc/CoreAuthBlocProvider";
+import { CoreAppBlocProvider } from "src/modules/core/infrastructure/bloc/CoreAppBlocProvider";
+import { initialAppState } from "src/modules/core/domain/CoreAppState";
+import { initialAuthState } from "src/modules/core/domain/CoreAuthState";
 
-const wrap = (
-  ui: ReactElement,
-  initialState?: CoreAuthBlocProviderProps["initialState"],
-  initialEntries?: LocationDescriptor<unknown>[],
-  options?: RenderOptions
-) =>
-  render(ui, {
+const wrap = () =>
+  render(<FormLogin />, {
     wrapper: ({ children }) => (
-      <MemoryRouter initialEntries={initialEntries ?? ["/"]}>
-        <CoreAuthBlocProvider initialState={initialState}>
-          {children}
-        </CoreAuthBlocProvider>
+      <MemoryRouter initialEntries={["/"]}>
+        <CoreAppBlocProvider initialState={initialAppState}>
+          <CoreAuthBlocProvider initialState={initialAuthState}>
+            {children}
+          </CoreAuthBlocProvider>
+        </CoreAppBlocProvider>
       </MemoryRouter>
     ),
-    ...options,
   });
 
 it("renders form login", () => {
-  const wrapper = wrap(<FormLogin />);
+  const wrapper = wrap();
   expect(wrapper.queryByTestId("form-login")).toBeInTheDocument();
 });
