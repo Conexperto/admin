4322e571a8f2e981ede231e61c409cdf2a4087c4
Fernando Fernandez <frfernandezdev@gmail.com>
2021-12-30 09:37:22 -0400

    test(core): added CoreAuthBloc test

---
+++ src/modules/core/infrastructure/bloc/__tests__/CoreAuthBloc.test.ts
@@ -0,0 +1,40 @@
+import { FirebaseAuthClientFactory } from "src/modules/shared/infrastructure/persistence/firebase/FirebaseAuthClientFactory";
+import { FirebaseClientFactory } from "src/modules/shared/infrastructure/persistence/firebase/FirebaseClientFactory";
+import { LocalStorageFactory } from "src/modules/shared/infrastructure/persistence/local-storage/LocalStorageFactory";
+import { FirebaseCoreAuthRepository } from "../../persistence/FirebaseCoreAuthRepository";
+import { CoreAppBloc } from "../CoreAppBloc";
+import { CoreAuthBloc } from "../CoreAuthBloc";
+import * as config from "src/modules/shared/infrastructure/config/adminsdk.json";
+
+jest.mock(
+  "src/modules/shared/infrastructure/persistence/firebase/FirebaseAuthClientFactory"
+);
+jest.mock(
+  "src/modules/shared/infrastructure/persistence/firebase/FirebaseClientFactory"
+);
+jest.mock("../../persistence/FirebaseCoreAuthRepository");
+jest.mock(
+  "src/modules/shared/infrastructure/persistence/local-storage/LocalStorageFactory"
+);
+
+describe("CoreAuthBloc", () => {
+  let bloc: CoreAuthBloc;
+
+  FirebaseAuthClientFactory.createClient = jest.fn().mockReturnValue({});
+  FirebaseClientFactory.createClient = jest.fn().mockReturnValue({});
+
+  it("should be instance CoreAuthBloc", () => {
+    bloc = new CoreAuthBloc(
+      new FirebaseCoreAuthRepository(
+        FirebaseAuthClientFactory.createClient(
+          "admin",
+          FirebaseClientFactory.createClient("admin", config)
+        )
+      ),
+      new LocalStorageFactory(),
+      new CoreAppBloc()
+    );
+
+    expect(bloc).toBeInstanceOf(CoreAuthBloc);
+  });
+});
