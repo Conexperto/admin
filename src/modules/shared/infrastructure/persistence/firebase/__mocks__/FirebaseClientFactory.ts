437a1f88978a0113e8da1b0e4b40dd9da9480320
Fernando Fernandez <frfernandezdev@gmail.com>
2021-12-30 10:49:25 -0400

    test(shared): added static method to FirebaseClientFactory mock

--- src/modules/shared/infrastructure/persistence/firebase/__mocks__/FirebaseClientFactory.ts
+++ src/modules/shared/infrastructure/persistence/firebase/__mocks__/FirebaseClientFactory.ts
@@ -1,7 +1,10 @@
 import { FirebaseClientFactory } from "../FirebaseClientFactory";
 
-const FirebaseClientFactoryMock =
-  jest.createMockFromModule<FirebaseClientFactory>("../FirebaseClientFactory");
+const FirebaseClientFactoryMock = jest.createMockFromModule<
+  FirebaseClientFactory & { createClient: () => void }
+>("../FirebaseClientFactory");
+
+FirebaseClientFactoryMock.createClient = jest.fn();
 
 console.info("FirebaseClientFactoryMock");
 export { FirebaseClientFactoryMock as FirebaseClientFactory };
