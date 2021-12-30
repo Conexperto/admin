c100730f45db5af8695b7cca8a37235659dcc198
Fernando Fernandez <frfernandezdev@gmail.com>
2021-12-30 15:27:15 -0400

    test(core): implement using mocks and added describe

--- src/modules/core/presentation/pages/Login/index.test.tsx
+++ src/modules/core/presentation/pages/Login/index.test.tsx
@@ -1,11 +1,41 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import { CoreAppBlocProvider } from "src/modules/core/infrastructure/bloc/CoreAppBlocProvider";
+import { CoreAuthBlocProvider } from "src/modules/core/infrastructure/bloc/CoreAuthBlocProvider";
+import { initialAuthState } from "src/modules/core/domain/CoreAuthState";
+import { initialAppState } from "src/modules/core/domain/CoreAppState";
 import Login from ".";
 
-const wrap = () => render(<Login />);
+jest.mock(
+  "src/modules/shared/infrastructure/persistence/firebase/FirebaseAuthClientFactory"
+);
+jest.mock(
+  "src/modules/shared/infrastructure/persistence/firebase/FirebaseClientFactory"
+);
+jest.mock(
+  "src/modules/core/infrastructure/persistence/FirebaseCoreAuthRepository"
+);
+jest.mock(
+  "src/modules/shared/infrastructure/persistence/local-storage/LocalStorageFactory"
+);
 
-it("renders Login page", () => {
-  const wrapper = wrap();
+const wrap = () =>
+  render(<Login />, {
+    wrapper: ({ children }) => (
+      <MemoryRouter initialEntries={["/"]}>
+        <CoreAppBlocProvider initialState={initialAppState}>
+          <CoreAuthBlocProvider initialState={initialAuthState}>
+            {children}
+          </CoreAuthBlocProvider>
+        </CoreAppBlocProvider>
+      </MemoryRouter>
+    ),
+  });
 
-  expect(wrapper.queryByTestId("page-login")).toBeInTheDocument();
+describe("Login", () => {
+  it("renders Login page", () => {
+    wrap();
+    expect(screen.queryByTestId("page-login")).toBeInTheDocument();
+  });
 });
