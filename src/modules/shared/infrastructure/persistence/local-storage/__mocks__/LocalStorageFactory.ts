0b8eba4c792bb7fe7db3a5f56b2049c9e3ce4089
Fernando Fernandez <frfernandezdev@gmail.com>
2021-12-30 10:29:32 -0400

    test(shared): fix issue with constructor in LocalStorageFactoryMock

--- src/modules/shared/infrastructure/persistence/local-storage/__mocks__/LocalStorageFactory.ts
+++ src/modules/shared/infrastructure/persistence/local-storage/__mocks__/LocalStorageFactory.ts
@@ -1,27 +1,7 @@
-const LocalStorage = jest.createMockFromModule<{ [key: string]: any }>(
-  "../LocalStorageFactory"
+import { LocalStorageFactory } from "../LocalStorageFactory";
+
+const LocalStorageFactoryMock = jest.fn(() =>
+  jest.createMockFromModule<LocalStorageFactory>("../LocalStorageFactory")
 );
 
-let localstorage: { [key: string]: any } = {};
-
-LocalStorage.add = (key: string, value: string) => {
-  console.info("LocalStorage add");
-  localstorage[key] = value;
-};
-
-LocalStorage.get = (key: string) => {
-  console.info("LocalStorage get");
-  return localstorage[key];
-};
-
-LocalStorage.remove = (key: string) => {
-  console.info("LocalStorage remove");
-  delete localstorage[key];
-};
-
-LocalStorage.clear = () => {
-  console.info("LocalStorage clear");
-  localstorage = {};
-};
-
-export default LocalStorage;
+export { LocalStorageFactoryMock as LocalStorageFactory };
