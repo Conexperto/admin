c8f1bc05e3f95b970fef87c40c980432beb8482d
Fernando Fernandez <frfernandezdev@gmail.com>
2021-12-30 16:54:10 -0400

    feat(core): added updateTitle

--- src/modules/core/presentation/pages/Login/index.tsx
+++ src/modules/core/presentation/pages/Login/index.tsx
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useCoreApp } from "src/modules/core/infrastructure/bloc/CoreAppBlocProvider";
 import FormLogin from "../../organisms/FormLogin";
 import TemplateUnauthorized from "../../templates/TemplateUnauthorized";
 
 const Login: React.FC = () => {
+  const { bloc } = useCoreApp();
+
+  useEffect(() => {
+    bloc.updateTitle("Iniciar Sesion");
+  }, []);
+
   return (
     <TemplateUnauthorized>
       <div data-testid="page-login">
