3e46b4d6242ea8f0422f188206f9b87d79ecc079
Fernando Fernandez <frfernandezdev@gmail.com>
2021-12-30 10:30:48 -0400

    test(core): added method mock to FirebaseCoreAuthRepository mock

--- src/modules/core/infrastructure/persistence/__mocks__/FirebaseCoreAuthRepository.ts
+++ src/modules/core/infrastructure/persistence/__mocks__/FirebaseCoreAuthRepository.ts
@@ -1,9 +1,18 @@
 import { FirebaseCoreAuthRepository } from "../FirebaseCoreAuthRepository";
 
-const FirebaseCoreAuthRepositoryMock = jest.fn(() =>
-  jest.createMockFromModule<FirebaseCoreAuthRepository>(
+const FirebaseCoreAuthRepositoryMock = jest.fn(() => {
+  const proto = jest.createMockFromModule<FirebaseCoreAuthRepository>(
     "../FirebaseCoreAuthRepository"
-  )
-);
+  );
+
+  proto.onAuthStateChanged = jest.fn();
+  proto.onIdTokenChanged = jest.fn();
+  proto.createUserWithEmailAndPassword = jest.fn();
+  proto.signInWithEmailAndPassword = jest.fn();
+  proto.currentUser = jest.fn();
+  proto.signOut = jest.fn();
+
+  return proto;
+});
 
 export { FirebaseCoreAuthRepositoryMock as FirebaseCoreAuthRepository };
