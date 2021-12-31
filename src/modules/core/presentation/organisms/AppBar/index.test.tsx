ae59109028d0fa6763c0c5ceee61d82e61ba99ee
Fernando Fernandez <frfernandezdev@gmail.com>
2021-12-30 22:43:08 -0400

    test(core): added test by event

--- src/modules/core/presentation/organisms/AppBar/index.test.tsx
+++ src/modules/core/presentation/organisms/AppBar/index.test.tsx
@@ -4,14 +4,18 @@ import {
   fireEvent,
   render,
   RenderOptions,
+  waitForElementToBeRemoved,
+  waitFor,
+  cleanup,
 } from "@testing-library/react";
-import { ReactElement } from "react";
+import { ReactElement, useEffect } from "react";
 import { LocationDescriptor } from "history";
 import { MemoryRouter } from "react-router-dom";
 import AppBar from ".";
 import {
   CoreAppBlocProvider,
   CoreAppBlocProviderProps,
+  useCoreApp,
 } from "src/modules/core/infrastructure/bloc/CoreAppBlocProvider";
 import { CoreAuthBlocProvider } from "src/modules/core/infrastructure/bloc/CoreAuthBlocProvider";
 import { initialAuthState } from "src/modules/core/domain/CoreAuthState";
@@ -53,7 +57,9 @@ const wrap = (
   });
 
 describe("AppBar", () => {
-  it("renders appbar by default", async () => {
+  afterEach(cleanup);
+
+  it("renders appbar by default", () => {
     wrap(<AppBar />);
     expect(screen.queryByTestId("appbar")).toBeInTheDocument();
     expect(screen.queryByTestId("title")).toHaveTextContent(
@@ -61,28 +67,68 @@ describe("AppBar", () => {
     );
   });
 
-  it("should open overflowMenu", () => {
-    act(() => {
-      wrap(<AppBar />);
-    });
-    fireEvent.click(screen.getByTestId("more-actions"));
-    act(() => {
-      const overflowMenu = screen.queryByTestId("overflow-menu");
-      expect(overflowMenu).toBeInTheDocument();
-    });
+  it("renders overflowMenu opened", () => {
+    wrap(<AppBar />, { ...initialAppState, overflowMenu: document.body });
+
+    const overflowMenu = screen.queryByTestId("overflow-menu");
+    expect(overflowMenu).toBeInTheDocument();
   });
 
-  it("should close overflowMenu", () => {
-    const initialState: CoreAppState = { ...initialAppState };
-    initialState.overflowMenu = document.body;
-    wrap(<AppBar />, initialState);
+  it("renders overflowMenu opened", () => {
+    wrap(<AppBar />, { ...initialAppState, overflowMenu: null });
+
+    const overflowMenu = screen.queryByTestId("overflow-menu");
+    expect(overflowMenu).not.toBeInTheDocument();
+  });
+
+  it("should open the overflowMenu by context", () => {
+    const TestComponent: React.FC = () => {
+      const { bloc } = useCoreApp();
+
+      useEffect(() => bloc.openOverflowMenu(document.body), []);
+
+      return <AppBar />;
+    };
+    wrap(<TestComponent />, {
+      ...initialAppState,
+      overflowMenu: null,
+    });
+
+    const overflowMenu = screen.queryByTestId("overflow-menu");
+    expect(overflowMenu).toBeInTheDocument();
+    expect(overflowMenu).toBeVisible();
+  });
+
+  it("should close the overflowMenu by context", () => {
+    const TestComponent: React.FC = () => {
+      const { bloc } = useCoreApp();
+
+      useEffect(() => bloc.closeOverflowMenu(), []);
+
+      return <AppBar />;
+    };
+    wrap(<TestComponent />, {
+      ...initialAppState,
+      overflowMenu: document.body,
+    });
+
+    const overflowMenu = screen.queryByTestId("overflow-menu");
+    expect(overflowMenu).not.toBeInTheDocument();
+  });
+
+  it("should open the overflowMenu by event", () => {
+    wrap(<AppBar />, { ...initialAppState, overflowMenu: null });
 
     fireEvent.click(screen.getByTestId("more-actions"));
-    act(() => {
-      setTimeout(() => {
-        const overflowMenu = screen.queryByTestId("overflow-menu");
-        expect(overflowMenu).not.toBeInTheDocument();
-      });
-    });
+
+    const overflowMenu = screen.queryByTestId("overflow-menu");
+    expect(overflowMenu).toBeInTheDocument();
+  });
+
+  it("should close the overflowMenu by event", () => {
+    wrap(<AppBar />, { ...initialAppState, overflowMenu: document.body });
+    fireEvent.click(screen.getByTestId("more-actions"));
+
+    waitForElementToBeRemoved(() => screen.queryByTestId("overflow-menu"));
   });
 });
